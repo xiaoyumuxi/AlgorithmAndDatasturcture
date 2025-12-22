ccb605066d91256643093ab511a25342c44e6439
yaojingxi <23722032@bjtu.edu.cn>
2025-12-22 10:14:34 +0800

    Bellman_ford算法，最基础的版本——关键的思想是对边进行松弛n-1(n-1次以上也是没有问题的，但是多松弛的轮数里面的minDist数组不会进行改变，属于性能浪费)

---
+++ src/main/java/Utils/Graph/Bellman_ford.java
@@ -0,0 +1,74 @@
+package Utils.Graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Bellman_ford {
+    static class Edge {
+        int src, dest, weight;
+
+        public Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    static int V;
+    static int E;
+    static List<Edge> edges = new ArrayList<>();
+    static int[] minDist;
+    static int start = 1;
+    static int end;
+
+    public Bellman_ford(int V, int E) {
+        Bellman_ford.V = V;
+        Bellman_ford.E = E;
+    }
+
+    public static void addEdge(int src, int dest, int weight) {
+        edges.add(new Edge(src, dest, weight));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        V = scanner.nextInt();
+        E = scanner.nextInt();
+        // 初始化minDist数组
+        minDist = new int[V + 1];
+        end = V;
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+        minDist[start] = 0;//源点到源点的最短距离为0
+        
+        for(int i = 0; i < E; i++){
+            int src = scanner.nextInt();
+            int dest = scanner.nextInt();
+            int weight = scanner.nextInt();
+            addEdge(src, dest, weight);
+        }
+        // 对所有的边松弛V-1次
+        for(int i = 1; i < V; i++){
+            boolean updated = false; // 优化：检测是否发生了更新
+            for(Edge edge : edges){
+                int src = edge.src;
+                int dest = edge.dest;
+                int weight = edge.weight;
+                if(minDist[src] != Integer.MAX_VALUE && minDist[src] + weight < minDist[dest]){
+                    // src不可以为MAX，否则就不可以基于这个来对边进行更新
+                    minDist[dest] = minDist[src] + weight;
+                    updated = true;
+                }
+            }
+            // 如果这一轮没有发生更新，可以提前结束
+            if (!updated) break;
+        }
+        
+        if(minDist[end] == Integer.MAX_VALUE){
+            System.out.println("不能到达终点");
+        } else {
+            System.out.println(minDist[end]);
+        }
+    }
+}
