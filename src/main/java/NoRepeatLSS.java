1f4ad78bb1dc219c5ad720112a411a345f4a1127
yaojingxi <23722032@bjtu.edu.cn>
2026-03-05 17:21:18 +0800

    3

--- src/main/java/NoRepeatLSS.java
+++ src/main/java/NoRepeatLSS.java
@@ -1,19 +1,23 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
-public class LSS {
+public class NoRepeatLSS {
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(getLenOfLSS(s));
+        sc.close();
     }
 
-    public static int lengthOfLongestSubstring(String s) {
+    public static int getLenOfLSS(String s) {
         if (s == null || s.length() == 0) return 0;
-
         char[] ch = s.toCharArray();
-        int maxLen = 0;
         Set<Character> set = new HashSet<>();
-        for (int j = 0, i = 0; j < ch.length; j++) {
+        int i = 0;
+        int maxLen = 0;
+        for (int j = 0; j < ch.length; j++) {
             while (set.contains(ch[j])) {
                 set.remove(ch[i]);
                 i++;
