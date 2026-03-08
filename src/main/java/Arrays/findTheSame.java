c9fb885b297eb4e663601fdb6a34b9981d2ccd00
yaojingxi <23722032@bjtu.edu.cn>
2026-03-08 21:28:59 +0800

    关产提示的数字信息来时间链表的判环思路

--- src/main/java/Arrays/findTheSame.java
+++ src/main/java/Arrays/findTheSame.java
@@ -10,6 +10,8 @@ public class findTheSame {
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
+        System.out.println(find(nums));
+        System.out.println(find1(nums));
         sc.close();
     }
     
@@ -22,5 +24,24 @@ public class findTheSame {
         return -1;
     }// O(N^2)复杂度，有点高了需要进行优化
 
+    public static int find1(int[] nums){
+        // 使用双指针--快慢指针的方法来进行遍历,因为题目数据里面有nums[i] <= n
+        int slow = nums[0];
+        int fast = nums[nums[0]];
 
+        while(fast != slow){
+            slow = nums[slow];           // 走一步
+            fast = nums[nums[fast]];     // 走两步
+        }
+
+        // 相遇之后重置一个节点
+        slow = nums[0];
+
+        while(fast != slow){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;//这个时候再相遇就是在一个重复的环的入口处了，也就是重复的数
+    }
 }
