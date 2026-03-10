1a5680a4f0f6df990469204470189ffc2561ff81
yaojingxi <23722032@bjtu.edu.cn>
2026-03-10 13:18:11 +0800

    42.接雨水 编码错误1:在这里求左侧最大数组里面没有直接利用到数组本身的含义进行处理，然后后面自己处理的时候又频繁出错 编码错误2:水位的意思是需要减去里面的柱子的高度的height[i]的 编码错误3:这里的计算rightMax[i-1]用的是 height[i] 但是实际应该是 height[i - 1]

---
+++ src/main/java/TwoPointer/GetTheRain.java
@@ -0,0 +1,44 @@
+package TwoPointer;
+
+import java.util.Scanner;
+
+public class GetTheRain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+
+        str = str.substring(1, str.length() - 1);
+        String[] parts = str.split(",");
+
+        int[] height = new int[parts.length];
+        for (int i = 0; i < parts.length; i++) {
+            height[i] = Integer.parseInt(parts[i].trim());
+        }
+
+        System.out.println(trap1(height));
+    }
+
+    public static int trap1(int[] height) {
+        // 目的是记录每一个位置左侧和右侧最高的柱子
+        int[] leftMax = new int[height.length];
+        leftMax[0] = height[0];
+        for(int i = 1; i < height.length ;i++){
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            // 编码错误1:在这里求左侧最大数组里面没有直接利用到数组本身的含义进行处理，然后后面自己处理的时候又频繁出错
+        }
+        int[] rightMax = new int[height.length];
+        rightMax[height.length -1] = height[height.length - 1];
+        for(int i = height.length - 1; i >0 ; i--){
+            rightMax[i - 1] = Math.max(rightMax[i], height[i - 1]);
+            // 编码错误3:这里的计算rightMax[i-1]用的是 height[i] 但是实际应该是 height[i - 1]
+        }
+        int res = 0;
+        for(int i = 0; i < height.length ; i++){
+            res +=Math.min(leftMax[i], rightMax[i])- height[i];
+            // 编码错误2:水位的意思是需要减去里面的柱子的高度的height[i]的
+        }
+        // 思路卡住了，突然忘记求这两个数组的意义是求出每一列最大容积
+        return res;
+    }
+}
