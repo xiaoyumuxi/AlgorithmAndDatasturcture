a0d1d99725e1794ff5fb80c351826c03884504ce
yaojingxi <23722032@bjtu.edu.cn>
2026-03-11 21:23:52 +0800

    思路错误1:单调递减的单调队列，每一次入一个元素都会导致队尾后面的比其小的元素被淘汰的 关键就是维护一个单调递减队列来处理这些事情的呀

--- src/main/java/MovingWindows/MaxMoveWindows.java
+++ src/main/java/MovingWindows/MaxMoveWindows.java
@@ -1,37 +1,45 @@
 package MovingWindows;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class MaxMoveWindows {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc. nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n ;i++){
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(Arrays.toString(maxSlidingWindow(nums, k)));
+        sc.close();
     }
     
     public static int[] maxSlidingWindow(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        int[] max = new int[nums.length - k + 1];//max里面存的只最大值的索引
-        int MaxNum = Integer.MIN_VALUE;
-        for(int i = 0 ; i < k ; i++){
-            if(nums[i] > MaxNum){
-                MaxNum = nums[i];
-                max[0] = i;
-            }
-            map.put(i, nums[i]);
-        }
-        // 开始维护滑动的流程
-        int i = 1;
+        // 构建一个存Index的单调队列
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
         Deque<Integer> queue = new ArrayDeque<>();
-        for(int j = k - 1 ; j < nums.length ; j++){
-            map.put(j,nums[j]);//刚进来右边扩展
-            map.remove(i);//左边需要缩小
-            // 需要维护当前窗口的最大值
-            if(max[i-1] >= i && max[i-1] <=j )max[i] = max[i-1];//保持不变
-            if(max[i-1] < i){
-                // 这个时候是需要进行处理的，感觉是需要一个堆结构的
+        int idx = 0;
+        for (int i = 0; i < n; i++) {
+            while(!queue.isEmpty() && queue.peekFirst() <= i - k)queue.pollFirst();//将过期的弹出去
+
+            while(!queue.isEmpty() && nums[queue.peekLast()] <= nums[i])queue.pollLast();
+            //当前新来的元素 nums[i]，会把队尾所有比它小的元素全部淘汰掉
+            // 思路错误1:单调递减的单调队列，每一次入一个元素都会导致队尾后面的比其小的元素被淘汰的
+            
+            queue.offerLast(i);
+
+            if (i >= k - 1) {
+                ans[idx++] = nums[queue.peekFirst()];
             }
+
         }
+
+        return ans;
+
     }
 }
