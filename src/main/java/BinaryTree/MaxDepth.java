954c418f581c8cc8464c6303e4b1b42b609270e6
yaojingxi <23722032@bjtu.edu.cn>
2026-03-23 13:46:10 +0800

    104的迭代法，BFS版本，还是写得不够熟练 思路错误1:不知道怎么记录层数，其实是求队列里面所有的元素，将所有元素的子元素全部入队之后就可以进入下一层

--- src/main/java/BinaryTree/MaxDepth.java
+++ src/main/java/BinaryTree/MaxDepth.java
@@ -1,5 +1,8 @@
 package BinaryTree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class MaxDepth {
     public static class TreeNode {
         int val;
@@ -23,10 +26,28 @@ public class MaxDepth {
         root.right.left = new TreeNode(15);
         root.right.right = new TreeNode(7);
         System.out.println(getTheMaxDepth(root));
+        System.out.println(bfs(root));
     }
 
     public static int getTheMaxDepth(TreeNode root){
         if(root == null)return 0;
         return Math.max(getTheMaxDepth(root.left), getTheMaxDepth(root.right)) + 1;
+    }
+
+    public static int bfs(TreeNode root){
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int depth = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            // 思路错误1:不知道怎么记录层数，其实是求队列里面所有的元素，将所有元素的子元素全部入队之后就可以进入下一层
+            for(int i = 0; i < size ; i++){
+                TreeNode p = queue.poll();
+                if(p.left!=null)queue.offer(p.left);
+                if (p.right!=null)queue.offer(p.right);
+            }
+            depth++;
+        }
+        return depth;
     }
 }
