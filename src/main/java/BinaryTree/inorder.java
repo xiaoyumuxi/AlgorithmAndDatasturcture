9006b3fbb3cc7e3fb07f57972341307965b41eb6
yaojingxi <23722032@bjtu.edu.cn>
2026-03-23 13:20:36 +0800

    迭代法还是不够熟练甚至还有错误 编码错误2:这里是需要两个大的while的，而不是平级的两个小while

--- src/main/java/BinaryTree/inorder.java
+++ src/main/java/BinaryTree/inorder.java
@@ -1,6 +1,8 @@
 package BinaryTree;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class inorder {
@@ -25,6 +27,7 @@ public class inorder {
         root.right = new TreeNode(2);
         root.right.left = new TreeNode(3);
         System.out.println(inorderTraversal(root));
+        System.out.println(inorderTraversal1(root));
     }
 
     public static List<Integer> inorderTraversal(TreeNode root){
@@ -35,5 +38,24 @@ public class inorder {
         res.add(root.val);
         res.addAll(inorderTraversal(root.right));
         return res;
+    }
+
+    public static List<Integer> inorderTraversal1(TreeNode root){
+        // 使用迭代法来实现,左中右
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            // 刚开始的时候栈是空的，如果只有一个stack非空就进不了这个循环
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+            // 编码错误2:这里是需要两个大的while的，而不是平级的两个小while
+        }
+        return list;
     }
 }
