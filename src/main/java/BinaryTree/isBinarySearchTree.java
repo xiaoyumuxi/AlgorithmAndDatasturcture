7ed40a4d69384cd33ff5ce9faf4ab87a6ea42c5c
yaojingxi <23722032@bjtu.edu.cn>
2026-03-24 16:53:49 +0800

    上下界方法来维护二叉搜索树

--- src/main/java/BinaryTree/isBinarySearchTree.java
+++ src/main/java/BinaryTree/isBinarySearchTree.java
@@ -24,8 +24,10 @@ public class isBinarySearchTree {
         root.right.left = new TreeNode(3);
         root.right.right = new TreeNode(6);
         System.out.println(isSearchTree(root));
+        System.out.println(isSearchTree1(root, Integer.MIN_VALUE, Integer.MAX_VALUE));
     }
 
+    // 思路问题:这个方法的，因为是不断检查每一个节点下面的子树因此就会导致反复的监测从而导致很多重复的操作
     public static boolean isSearchTree(TreeNode root){
         // 保证左子树最右边的需要小于root.val然后还需要保证右子树最左边的大于root.val
         if (root == null) return true;
@@ -47,5 +49,12 @@ public class isBinarySearchTree {
             return isSearchTree(root.left) && isSearchTree(root.right);
         }
         return false;
+    }
+
+    // 这个优化就导致不用反复的去进行遍历了，而是使用min和max去维护这个树的合法性，上下界方法
+    public static boolean isSearchTree1(TreeNode root,int min,int max){
+        if(root == null)return true;
+        if(root.val < min || root.val > max)return false;
+        return isSearchTree1(root.left, min, root.val) && isSearchTree1(root.right, root.val, max);
     }
 }
