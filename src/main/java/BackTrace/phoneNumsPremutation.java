0ab6a5f62f6b2acf02d8b52d208e01783fc2e94e
yaojingxi <23722032@bjtu.edu.cn>
2026-03-26 15:49:26 +0800

    add

--- src/main/java/BackTrace/phoneNumsPremutation.java
+++ src/main/java/BackTrace/phoneNumsPremutation.java
@@ -33,7 +33,9 @@ public class phoneNumsPremutation {
     public static void dfs(char[] ch,int level){
         if(level == ch.length){
             res.add(sb.toString());
+            // 这里其实也是使用了new的复制副本的方式的，因为toString转化之后就是新的东西了
             return;
+            // 这里需要return，不要因为void就忽视了return，只要不是最后return，否则都是需要写的
         }
 
         String a = map.get(ch[level]); // 表示当前位置可以选择的字符串
